8810b64e31997145bcc97147c2a07c69ab144e09
Lukasz Komsta <lukaszkomsta@yahoo.com>
2020-05-28 11:52:49 +0100

    Snake is moving now, borders not coded

--- Unit1.cpp
+++ Unit1.cpp
@@ -4,6 +4,7 @@
 #pragma hdrstop
 
 #include "Unit1.h"
+#include "mmsystem.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -25,30 +26,33 @@ void resetTimers()
     Form1->down->Enabled = false;
 
  }
-//---------------------------------------------------------------------------
- void setGame()
-{
-
-    //placing fruit on the field
+ //----------------------------------------------------------------------------
+ void setFruitLocation()
+ {
+    //randomly choosing fruit place on the field
         randomize();
         xFruit = random(400);
         yFruit = random(400);
 
-        //prevent placing fruit on middle of the game field where will be snake at the beginning of the game
+    //prevent placing fruit on middle of the game field where will be snake at the beginning of the game
         if((xFruit > 180 && xFruit < 220) && (yFruit > 180 && yFruit < 220) )
           {
                 xFruit == 100;
           }
+    //locatiing fruit on the field
         Form1->fruit->Left = xFruit;
         Form1->fruit->Top =  yFruit;
-
+ }
+//---------------------------------------------------------------------------
+ void setGame()
+{
+        setFruitLocation();
         Form1->head->Left = 200;
         Form1->head->Top = 200;
 
         //play intro sound
-        ///sndPlaySound("snd/intro.wav",SND_ASYNC);
-        //resetTimers();
-
+        sndPlaySound("snd/intro.wav",SND_ASYNC);
+        resetTimers();
 
 }
 //---------------------------------------------------------------------------
@@ -75,40 +79,41 @@ void __fastcall TForm1::upTimer(TObject *Sender)
 //---------------------------------------------------------------------------
 void __fastcall TForm1::FormCreate(TObject *Sender)
 {
-setGame();
+        setGame();
 }
 //---------------------------------------------------------------------------
 
-void __fastcall TForm1::FormKeyPress(TObject *Sender, char &Key)
+void __fastcall TForm1::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
 {
-if(Key == VK_LEFT)
-{
- head->Picture->LoadFromFile("img/lebWlewo");
- resetTimers();
- left->Enabled = true;
+  if(Key == VK_UP)
+  {
+        head->Picture->LoadFromFile("img/leb.bmp");
+        resetTimers();
+        up->Enabled = true;
 
-}
-if(Key == VK_DOWN)
-{
- head->Picture->LoadFromFile("img/lebWdol");
- resetTimers();
- down->Enabled = true;
+   }
+  if(Key == VK_LEFT)
+  {
+        head->Picture->LoadFromFile("img/lebWlewo.bmp");
+        resetTimers();
+        left->Enabled = true;
+   }
 
-}
+  if(Key == VK_DOWN)
+ {
+        head->Picture->LoadFromFile("img/lebWdol.bmp");
+        resetTimers();
+        down->Enabled = true;
+ }
  if(Key == VK_RIGHT)
-{
- head->Picture->LoadFromFile("img/lebWprawo");
- resetTimers();
- right->Enabled = true;
+ {
+       head->Picture->LoadFromFile("img/lebWprawo.bmp");
+       resetTimers();
+       right->Enabled = true;
+ }
 
-}
-if(Key == VK_UP)
-{
- head->Picture->LoadFromFile("img/leb");
- resetTimers();
- up->Enabled = true;
 
-}
 }
 //---------------------------------------------------------------------------
 
